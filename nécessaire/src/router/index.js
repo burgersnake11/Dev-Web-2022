34098baedf9b907b8d05a42ddd09e4bcfa50aaeb
SebastianHac <64152057+SebastianHac@users.noreply.github.com>
2022-04-12 18:58:32 +0200

    Premier commit après liaison FE/API/BE réussie
    La liaison FE/ABI/BE fonctionne dans l'état actuel pour l'US 02. Les fichiers présents dans "nécessaires" sont tous les fichiers qui vont évoluer au fur et à mesure du projet : 
    - le "src" est le répertoire du projet Vue
    - le "main.py" est le fichier python qui contient les requêtes de notre API
    - le "model.py" est le fichier que nous utilisons pour créer nos schémas de données (pour la DB)

---
+++ nécessaire/src/router/index.js
@@ -0,0 +1,39 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/fiches',
+    name: 'fiches',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/FichesView.vue')
+  },
+  {
+  path: '/contact',
+  name: 'contact',
+  component: () => import('../views/ContactView.vue')
+  },
+  {
+    path: '/compte',
+    name: 'compte',
+    component: () => import('../views/CompteView.vue')
+    }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
