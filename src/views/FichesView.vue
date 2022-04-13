4c9f2a37b64093c9ade0ecd591f4c91889fb8482
SebastianHac <64152057+SebastianHac@users.noreply.github.com>
2022-04-13 19:17:27 +0200

    Changement structure des cartes
    Test de structure avec des listes par groupe d'enfants.

--- src/views/FichesView.vue
+++ src/views/FichesView.vue
@@ -2,12 +2,12 @@
   <v-app id="fiche">
     <h1>Fiches Médicales</h1>
     <v-main class="grey lighten-2">
-      <v-container class="my-5">
-        <v-layout row wrap>
-          <v-flex xs12 sm6 lg3 v-for="index in content" :key="index">
-            <v-flex v-if="index.id_groupe == 1">
-              <v-card class="text-center ma-3" >
-                  <v-card-text>
+      <v-flex xs12 sm6 lg3 v-for="(indice, i) in groupes" :key="i">
+        <v-card height="80">{{indice}}
+          <v-list v-for="index  in content" :key="index">
+            <v-list-tile v-if="index.id_groupe == i">
+              <v-card class="text-center ma-3">
+                 <v-card-text>
                       <div class="subheading">{{index.nom_enfant}} {{index.prenom_enfant}}</div>
                   </v-card-text>
                   <v-card-actions>
@@ -17,10 +17,10 @@
                       </v-btn>
                   </v-card-actions>
               </v-card>
-            </v-flex>
-          </v-flex>
-        </v-layout>
-      </v-container>
+            </v-list-tile>
+          </v-list>
+        </v-card>
+      </v-flex>
     </v-main>
   </v-app>
 </template>
@@ -33,6 +33,7 @@ export default {
     data(){
         return{
             content: null,
+            groupes: ["Les poussins", "Les Benjamines / Benjamins", "Les Etincelles / Chevalier", "Les Alpines / Conquérants", "Les Aventurières / Conquérants", "Les Grandes / Grands"]
         }
     },
     mounted(){
