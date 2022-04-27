a21351c37cd23b60a24d0cd554044ca38381bec6
zCargan <64102236+zCargan@users.noreply.github.com>
2022-04-27 17:40:14 +0200

    Add files via upload

---
+++ src/components/Cartes.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-app id="cartes">
+        <v-flex xs12 sm6 lg3 v-for="index in content" :key="index">
+            <v-card class="text-center ma-3" >
+
+                <v-card-text>
+                    <div class="subheading">{{index.nom_enfant}} {{index.prenom_enfant}}</div>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn flat color='grey lighten-1'>
+                    <v-icon small center>{{ic}}</v-icon>
+                    <span>Fiche</span>
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-flex>
+    </v-app>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    name:'cartes_enfants',
+    ic: "mdi-book",
+    data(){
+        return{
+            content: null,
+        }
+    },
+    mounted(){
+        axios
+        .get('http://localhost:8000/affichage')
+        .then((response) => {
+            this.content = response.data;
+            console.log(this.content);
+        });
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/FormulaireCompte.vue
@@ -0,0 +1,77 @@
+<template>
+    <div>
+        <h1>Créer votre compte!</h1>
+            <v-form>
+                <v-text-field
+                v-model="email_compte"
+                label="Adresse Email"
+                required
+                ></v-text-field>
+
+                <v-text-field
+                v-model="mdp_compte"
+                label="Mot de passe"
+                required
+                ></v-text-field>
+
+                <v-text-field
+                v-model="mdp_compte2"
+                label="Retaper votre mot de passe"
+                required
+                ></v-text-field>
+
+                <v-btn
+                class="mr-4"
+                @click="test"
+                >
+                Créer mon compte!
+                </v-btn>
+                <br>
+                <br>
+                <p class="font-weight-thin">
+                Vous avez déja un compte?
+                </p>
+                <v-btn to="/connection">Je me connecte !</v-btn>
+            </v-form>
+    </div>
+</template>
+
+
+
+<script>
+import axios from 'axios'
+export default {
+    data() {
+        return {
+            email_compte: '',
+            mdp_compte: '',
+            mdp_compte2: '',
+        }
+    },
+    methods : {
+        test() {
+            
+            if (this.mdp_compte != this.mdp_compte2) {
+                alert("Les deux mots de passe ne sont pas identiques");
+            } else {
+                let user = {
+                    email: this.email_compte,
+                    password: this.mdp_compte
+                }
+                axios
+                    .post("http://127.0.0.1:3000/api/auth/signup", user);
+                console.log(user)
+            }
+        }
+    }
+}
+</script>
+
+
+
+<style>
+
+</style>
+
+
+<!-- $v est nécessaire pour vuelidate -->
